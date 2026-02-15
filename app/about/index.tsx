e7e4fc1b2273dc6cf5c6e6b8dd54848970878d40
aparn-gupta <aparnagupta@Aparnas-MacBook-Air.local>
2026-02-15 21:50:07 +0530

    first commit

---
+++ app/about/index.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+
+
+export default function Index () {
+    return (
+        <View>
+
+            <Text> About Route </Text>
+
+        </View>
+    )
+}
---
+++ app/index.tsx
@@ -0,0 +1,30 @@
+import { Text, View } from "react-native";
+import "./globals.css";
+import {Link} from "expo-router";
+
+export default function Index() {
+
+  const links = [
+      "/about", "/explore", "/products"
+  ]
+
+
+
+  return (
+    <View className="p-5 flex justify-center items-center h-full"
+    >
+      <Text className="text-pink-900 font-semibold text-4xl">HHEHEHEH</Text>
+      {
+        links.map((item, i) => (
+            <Text key={i} className="py-3 text-fuchsia-800 font-bold"
+            >
+              <Link href={`/documents/${i}`}> Link {i + 1}</Link>
+            </Text>
+
+        ))
+      }
+    </View>
+  );
+}
+
+
