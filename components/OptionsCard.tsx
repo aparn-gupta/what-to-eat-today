f5bc9099d480b16661951a2701654102f90372d8
aparn-gupta <aparnagupta813@gmail.com>
2026-02-24 21:27:18 +0530

    dishcard rendering

--- components/OptionsCard.tsx
+++ components/OptionsCard.tsx
@@ -1,63 +1,43 @@
 import React, { JSX } from "react";
 import { Text, View } from "react-native";
 
-
-
-
 // @ts-ignore
-export default function OptionsCard ({optionItem, selected}): JSX.Element {
+export default function OptionsCard({ optionItem, selected }): JSX.Element {
+  return (
+    <>
+      <View
+        className={""}
+        style={{
+          backgroundColor: "white",
+          width: 144,
+          height: 144,
+          borderColor: "",
+          borderRadius: 8,
+          elevation: 5,
+          shadowColor: "",
+          shadowOpacity: 0.3,
+          shadowRadius: 4,
+          padding: 10,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      >
+      <View style={{backgroundColor: ""}}>
+      <Text
+          className={""}
+          style={{ color: "slategray", fontSize: 12, textAlign: "center" }}
+        >
+          {" "}
+          {optionItem.name}{" "}
+        </Text>
 
+        <Text className="text-lg " style={{color: "#D28E00", marginTop: 5}}> {selected[optionItem.value]}</Text>
+      </View>
 
-
-
-    return (
-
-        <>
-
-
-
-                <View  className={""} style={{
-                    backgroundColor: "white",
-                    width: 144,
-                    height: 144,
-                    borderColor: "",
-                    borderRadius: 8,
-                    elevation: 5,
-                    shadowColor: "",
-                    shadowOffset: { width: 0, height: 2 },
-                    shadowOpacity: 0.3,
-                    shadowRadius: 4,
-                    padding: 10,
-                    pointerEvents: "auto"
-                }}>
-
-
-                    <Text className={''} style={{color: "slategray", fontSize: 9, textAlign: "center"}}> {optionItem.name} </Text>
-
-                    <Text>  {selected[optionItem.value]}</Text>
-
-                </View>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        </>
-
-            )
-            }
-
-
-
+    
+      </View>
+    </>
+  );
+}
